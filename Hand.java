2056eb1af88b2c3c9ce232003453c1be70d8baca
Seth Hobson <sethlawrencehobson@gmail.com>
2024-05-20 21:20:14 -0400

    initial commit

---
+++ Hand.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Hand {
+
+  Die d1 = new Die();
+  Die d2 = new Die();
+
+  private ArrayList<Integer> hand = new ArrayList<>();
+
+  public ArrayList<Integer> newHand() {
+
+  }
+}
