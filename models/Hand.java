9ceffb8a13cfb4208165702e3597172efb499104
Seth Hobson <sethlawrencehobson@gmail.com>
2024-05-25 23:03:39 -0400

    cleaning up hand model

--- models/Hand.java
+++ models/Hand.java
@@ -1,37 +1,34 @@
 package models;
-
 import java.util.ArrayList;
-
 
 public class Hand {
   private ArrayList<Integer> dice = new ArrayList<>();
   private int score = 0;
 
 
-  public ArrayList<Integer> add(int die) {
 
-    dice.add(die);
 
-    return new ArrayList<>(dice);
+  public int add() {
+    Die d1 = new Die();
+
+    int roll = d1.roll();
+    dice.add(roll);
+    calculateScore();
+
+    return roll;
   }
 
-  private int calculateScore() {
-
+  private void calculateScore() {
     for (int dieVal : dice) {
       this.score += dieVal;
     }
-
-    return this.score;
   }
 
   public int getScore() {
-
-    this.score = this.calculateScore();
     return this.score;
   }
 
   public void announceScore() {
-    this.getScore();
     System.out.println("score: " + this.score);
   }
 
